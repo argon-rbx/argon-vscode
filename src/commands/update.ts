b9d788afb070bacbcdf03528eafbfbaa010f1aa0
santiarr <santiagoarredondocif@gmail.com>
2025-04-13 04:18:05 -0400

    Add extension self-update functionality

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -1,8 +1,15 @@
 import { commands } from "vscode"
 import * as argon from "../argon"
+import { updateExtension } from "../extension"
 
 export function update() {
-  return commands.registerCommand("argon.update", () => {
-    argon.update("all", false)
+  return commands.registerCommand("argon.update", async () => {
+    // Update CLI, plugin and templates (but not vscode since we'll handle that separately)
+    await argon.update("cli", false)
+    await argon.update("plugin", false)
+    await argon.update("templates", false)
+    
+    // Then try to update the extension itself
+    await updateExtension()
   })
 }
--- src/menu/update.ts
+++ src/menu/update.ts
@@ -1,12 +1,19 @@
 import * as argon from "../argon"
 import { Item } from "."
+import { updateExtension } from "../extension"
 
 export const item: Item = {
   label: "$(refresh) Update Lemonade",
-  description: "Update CLI and plugin components",
+  description: "Update CLI, plugin and extension components",
   action: "update",
 }
 
-export function run(): Promise<void> {
-  return argon.update("all", false)
+export async function run(): Promise<void> {
+  // Update CLI, plugin and templates (but not vscode since we'll handle that separately)
+  await argon.update("cli", false)
+  await argon.update("plugin", false)
+  await argon.update("templates", false)
+  
+  // Then try to update the extension itself
+  await updateExtension()
 }
